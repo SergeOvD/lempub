ee7d2020f17f4345869b42897ed9b89cbada2ea0
Serhii <sergeyovd@alumni.nottingham.ac.uk>
2018-08-15 11:51:42 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,30 @@
+#!/bin/sh -e
 
+echo "###################################################################################"
+echo "Please be Patient: Installation will start now..."
+echo "###################################################################################"
+
+cat << 'eof' >> /etc/nginx/sites-available/default
+server {
+	listen 80;
+	listen [::]:80;
+
+	server_name example.com www.example.com;
+
+	root /var/www/c;
+	index index.php index.html index.htm index.nginx-debian.html;
+
+	location / {
+		try_files $uri $uri/ =404;
+	}
+	
+	  location ~ \.php$ {
+        include snippets/fastcgi-php.conf;
+        fastcgi_pass unix:/run/php/php7.0-fpm.sock;
+    }
+
+    location ~ /\.ht {
+        deny all;
+    }
+}
+eof
